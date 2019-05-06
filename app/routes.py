a051a96cc7df96fac8e18bee81e25f14eabaebe7
antonioscardovi <antonio.scardovi@gmail.com>
2019-05-06 13:54:27 +0200

    importovi, izmjene u routes i index html

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from flask import render_template
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Travel.hr - Homepage')
+
+@app.route('/login')
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
