4a0a7017ac8de3cd8a186715adbf86ba11b2a611
JBrnelic <josip.brnelic@gmail.com>
2019-05-06 15:13:05 +0200

    Dodao nezavrsenu basu user

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from app import db
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(64), index=True) #Rola admin (2) ili user(1)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(64), index=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.String(64), db.ForeignKey('role.id'))
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
