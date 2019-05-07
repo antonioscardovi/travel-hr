9c4caebb68e9c335fcbebfa7f25c5e5d84ac2c25
antonioscardovi <antonio.scardovi@gmail.com>
2019-05-07 10:05:36 +0200

    skoro gotov login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
@@ -8,10 +8,14 @@ from app.forms import LoginForm
 def index():
     return render_template('index.html', title='Travel.hr - Homepage')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Login', form=form)
 
 
 @app.route('/register')
