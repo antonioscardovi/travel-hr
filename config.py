09bc213091c217c22235705933009bb4be7f379a
antonioscardovi <antonio.scardovi@gmail.com>
2019-05-09 15:28:53 +0200

    baza radi slike

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TRIP_UPLOAD_FOLDER = os.path.join(basedir + '/app/static/izlet_pics/')
+    PROFILE_UPLOAD_FOLDER = os.path.join(basedir + '/app/static/profile_pics/')
